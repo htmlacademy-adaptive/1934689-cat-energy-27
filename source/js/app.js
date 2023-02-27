3a321c6e84c06d8cd68406b7b843e6fd0b169f8f
Максим Рудой <doodoo690@gmail.com>
2023-02-27 20:18:45 +0500

    finish two pages index and catalog, adaptiv style grids

--- source/js/app.js
+++ source/js/app.js
@@ -1,26 +1,34 @@
-const headerBtn = document.querySelector('.header__button'),
-      bodyMenu = document.querySelector('.page__body')
-      headerNavDropDown = document.querySelector('.header__nav')
+const headerBtn = document.querySelector('.header__button');
+const bodyMenu = document.querySelector('.page__body');
+const headerNavDropDown = document.querySelector('.header__nav');
+
+headerBtn.addEventListener('click', () => {
+  const exp = "true" === headerBtn.getAttribute('aria-expanded')
+  headerBtn.setAttribute('aria-expanded', !exp)
 
 
-      headerBtn.addEventListener('click', () => {
-        const exp = "true" === headerBtn.getAttribute('aria-expanded')
-        headerBtn.setAttribute('aria-expanded', !exp)
+ headerBtn.classList.toggle('header__button--active')
+ bodyMenu.classList.toggle('page__body--menu')
+ headerNavDropDown.classList.toggle('header--drop-menu')
 
+ exp ? headerBtn.setAttribute('aria-label', 'Открыть меню')
+ : headerBtn.setAttribute('aria-label', 'Закрыть меню')});
 
-        headerBtn.classList.toggle('header__button--active')
-        bodyMenu.classList.toggle('page__body--menu')
-        headerNavDropDown.classList.toggle('header--drop-menu')
+document.addEventListener('DOMContentLoaded', function (Event) {
 
-        exp ? headerBtn.setAttribute('aria-label', 'Открыть меню')
-            : headerBtn.setAttribute('aria-label', 'Закрыть меню')
-      });
+  const  menuNoJs = document.querySelector('.menu')
+  const  headerButton = document.querySelector('.header__button')
+  const  headerNoJs = document.querySelector('.header__nav')
+  menuNoJs.classList.remove('menu--no-js'),
+  headerButton.classList.remove('header__button--no-js')
+  headerNoJs.classList.remove('header__nav--no-js')});
 
-            document.addEventListener('DOMContentLoaded', function (Event) {
-              const  menuNoJs = document.querySelector('.menu')
-              const  headerButton = document.querySelector('.header__button')
-              const headerNoJs = document.querySelector('.header__nav')
-              menuNoJs.classList.remove('menu--no-js'),
-            headerButton.classList.remove('header__button--no-js')
-            headerNoJs.classList.remove('header__nav--no-js')
-            });
+document.addEventListener("keydown", (evt) => {
+    if (evt.key === "Escape") {
+      headerNavDropDown.classList.remove('header--drop-menu');
+      bodyMenu.classList.remove('page__body--menu')
+      headerBtn.classList.remove('header__button--active')
+    }
+  }
+  );
+
